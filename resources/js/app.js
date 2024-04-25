f52518b9b739ea5562e4fbab9e35590028d24976
Andres Campanario <andres.campanario@cakedc.com>
2024-04-25 11:27:52 +0200

    replace require with import to adapt to ES6

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
     title: title => title ? `${title} - Vue App` : 'Vue App',
-    resolve: (name) => require(`./Components/${name}.vue`),
+    resolve: name => import(`./Components/${name}`),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
