c9dbdc180d01471283b4e87ededdb3c1fa0cd22a
Andres Campanario <andres.campanario@cakedc.com>
2024-11-04 12:07:28 +0100

    adapt example test pages and layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,15 +6,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 createInertiaApp({
     title: title => title ? `${title} - Vue App` : 'Vue App',
-    //resolve: name => {
-    //    const pages = import.meta.glob('./components/**/*.vue')
-    //    console.log(pages)
-    //    console.log(`./components/${name}.vue`)
-    //    return resolvePageComponent(`./components/${name}.vue`,pages)
-    //},
     resolve: name => resolvePageComponent(`./components/${name}.vue`, import.meta.glob('./components/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        console.log("Props recibidas: ", props);
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el);
