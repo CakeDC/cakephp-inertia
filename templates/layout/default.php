a17e1060efeec75e35f018906d7edb70bae77964
Andres Campanario <andres.campanario@cakedc.com>
2025-02-07 10:47:53 +0100

    add feather icons and restyle index view

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -8,5 +8,6 @@
     </head>
 <body>
     <?= $this->Inertia->component($page, 'app', ''); ?>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
 </body>
 </html>
